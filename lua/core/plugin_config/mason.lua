48b4562675eb6bbf47636018de3bdd37dde683c4
victro-rios <victrohugo100@gmail.com>
2025-11-27 17:12:00 -0800

    Adding typescript support

--- lua/core/plugin_config/mason.lua
+++ lua/core/plugin_config/mason.lua
@@ -3,5 +3,5 @@ vim.env.PATH = vim.env.PATH .. ":" .. vim.fn.stdpath("data") .. "/mason/bin"
 require("mason").setup()
 
 require("mason-lspconfig").setup({
-  ensure_installed = { "gopls", "lua_ls" },   -- mason will auto-install these
+  ensure_installed = { "gopls", "lua_ls", "ts_ls" },   -- mason will auto-install these
 })
