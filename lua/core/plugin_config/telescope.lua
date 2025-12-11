0f279ced57b134ea816f385a43218e068a29d6a1
victro-rios <victrohugo100@gmail.com>
2025-12-10 19:26:36 -0800

    Updating motion comments

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,5 +1,6 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<c-p>', builtin.find_files, {}) -- ctrl + p to look for files
-vim.keymap.set('n', '<Space>fg', builtin.live_grep, {}) -- ctrl + p to look for files
-vim.keymap.set('n', '<Space>fh', builtin.help_tags, {}) -- ctrl + p to look for files
+vim.keymap.set('n', '<Space>fg', builtin.live_grep, {}) -- space + f + g to look for strings between the files
+vim.keymap.set('n', '<Space>fh', builtin.help_tags, {}) -- space + f + h to look for documentation 
+
