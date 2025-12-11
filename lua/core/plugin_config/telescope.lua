bd6bc109b04de891abc1cf55705d0d53c2d19d1b
victro-rios <victrohugo100@gmail.com>
2025-12-10 19:38:35 -0800

    Adding go to definition

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -3,4 +3,6 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<c-p>', builtin.find_files, {}) -- ctrl + p to look for files
 vim.keymap.set('n', '<Space>fg', builtin.live_grep, {}) -- space + f + g to look for strings between the files
 vim.keymap.set('n', '<Space>fh', builtin.help_tags, {}) -- space + f + h to look for documentation 
-
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {}) -- go to definition
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {}) -- go to declaration 
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {}) -- go to implementation
